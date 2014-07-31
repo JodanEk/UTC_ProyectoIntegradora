d757b06d1c87b4c1e2b790b23cde99f0c283d662
root <root@amados.(none)>
2014-07-31 00:20:05 -0500

    se cambio en el archivo controlador_pizza.php, en el metodo que recibe los datos de la modificacion de la pizza

--- interfaz/controlador/controlador_pizza.php
+++ interfaz/controlador/controlador_pizza.php
@@ -1,14 +1,8 @@
 <?php 
 	require("../modelo/modelo_pizza.php");
 	$mPizza = new ModeloPizza();
-	/*$opcion = $_POST['opcion'];
-	switch($opcion){
-		case 'insertar':
-
-
-	}*/
 	
-	//------------------------VALORES PARA REGISTRAR UNA PIZZA-----------------------------------------------------
+	/*------------------------VALORES PARA REGISTRAR UNA PIZZA-----------------------------------------------------*/
 	if(isset($_POST["piz"]) && isset($_POST["pre"]) && isset($_POST["tam"])){
 		if($_POST["piz"]!="" && $_POST["pre"]!="" && $_POST["tam"]!=""){
 			$mPizza->addPizza($_POST["piz"], $_POST["pre"], $_POST["tam"]);
@@ -16,17 +10,18 @@
 	}
 	
 
-	//------------------------VALOR PARA ELIMINAR UN USUARIO-----------------------------------------------------
+	/*------------------------VALOR PARA ELIMINAR UN PIZZA-----------------------------------------------------*/
 	if(isset($_POST["id_del"])){
 		if($_POST["id_del"] != ""){
 			$mPizza->delPizza($_POST["id_del"]);
 		}
 	}
-	//------------------------VALOR PARA ACTUALIZAR UN USUARIO-----------------------------------------------------
+	/*------------------------VALORES PARA ACTUALIZAR UNA PIZZA-----------------------------------------------------*/
 	
-	if(isset($_POST['id_mod_p']) && isset($_POST['nom_mod_p']) && isset($_POST['pre_mod_p']) && isset($_POST['tam_mod_p']) &&){
-	if($_POST['id_mod_p'] != "" $_POST['nom_mod_p'] != "" $_POST['pre_mod_p'] != "" $_POST['tam_mod_p'] != "" ){
-			$mPizza->updPizza($_POST['id_mod_p'], $_POST['nom_mod_p'], $_POST['pre_mod_p'] ,$_POST['tam_mod_p'] );
+	
+	if(isset($_POST["id_mod_p"]) && isset($_POST["nom_mod_p"]) && isset($_POST["pre_mod_p"]) && isset($_POST["tam_mod_p"])){
+	if($_POST["id_mod_p"] != "" && $_POST["nom_mod_p"] != "" && $_POST["pre_mod_p"] != "" && $_POST["tam_mod_p"] != "" ){
+			$mPizza->updPizza($_POST["id_mod_p"], $_POST["nom_mod_p"], $_POST["pre_mod_p"] ,$_POST["tam_mod_p"] );
 		}
 	}
 
@@ -35,4 +30,7 @@
 			$mPizza->mostrarPizza();
 		}
 	}
+
 ?>
+
+	
